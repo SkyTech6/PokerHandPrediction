a9df3b093b4e0ab6167bfbec4aad43a845333d97
SkyCopeland <sky@skycopeland.com>
2021-03-28 17:23:38 -0400

    Adam Optimizer and Dropout Layers (65%)

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import warnings
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import regularizers
 import matplotlib.pyplot as plot
 
 warnings.filterwarnings('ignore')
@@ -24,29 +25,38 @@ test = pandas.get_dummies(hand_test)
 drop_train = poker_train.drop('Poker Hand', axis=1)
 drop_test = poker_test.drop('Poker Hand', axis=1)
 
+print('Training Set: ', drop_train.shape)
+print('Testing Set: ', drop_test.shape)
+
 # Build Neural Network with TensorFlow's Keras support
 model = keras.Sequential(
     [
         layers.Dense(5 * 16, activation='relu', input_dim=10, name='dense'),
-        # layers.Dropout(0.5),
-        # layers.Dense(5 * 16, activation='relu', name='dense'),
-        # layers.Dropout(0.2),
+        layers.Dropout(0.5),
+        layers.Dense(5 * 16, activation='relu', name='dense-drop'),
+        layers.Dropout(0.2),
         # layers.Dense(5 * 8, activation='relu', name='dense-deep'),
         # layers.Dropout(0.2),
         layers.Dense(10, activation='softmax', name='output')
+
+        # layers.Dense(15, activation="relu", input_dim=10),
+        # layers.Dense(10, activation="relu"),
+        # layers.Dense(10, activation="softmax")
     ]
 )
 
-loss_function = keras.losses.SparseCategoricalCrossentropy()
+# loss_function = keras.losses.SparseCategoricalCrossentropy()
+loss_function = keras.losses.binary_crossentropy
 opt = SGD(lr=0.01, momentum=0.9)
-model.compile(loss=loss_function, optimizer=opt, metrics=['accuracy'])
+adam = keras.optimizers.Adam()
+model.compile(loss=loss_function, optimizer=adam, metrics=['accuracy'])
 
-history = model.fit(drop_train, hand_train, epochs=100, batch_size=256, verbose=1,
-                    validation_data=(drop_test, hand_test), shuffle=True)
+history = model.fit(drop_train, train, epochs=1000, batch_size=256, verbose=1,
+                    validation_data=(drop_test, test), shuffle=True)
 
 
 if __name__ == '__main__':
-    score = model.evaluate(drop_test, hand_test, batch_size=256)
+    score = model.evaluate(drop_test, test, batch_size=256)
     model.summary()
     # plot loss during training
     plot.subplot(211)
